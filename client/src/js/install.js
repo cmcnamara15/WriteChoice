71686f6cd92db402cf2b5b2577756be8e5ab65c6
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-18 18:57:23 -0500

    Continue work on event handlers in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,19 +3,26 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent Chrome from showing the mini-infobar
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
     event.preventDefault();
-
-    // Stash the event so it can be triggered later
+  // Stash the event so it can be triggered later.
     deferredPrompt = event;
-
-    // Update UI notify the user they can install the PWA
+  // Update UI notify the user they can add to home screen
     butInstall.style.display = 'block';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Log the result
+    console.log(`User response to the install prompt: ${outcome}`);
+    // Hide the button
+    butInstall.style.display = 'none';
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
 });
 
 // TODO: Add an handler for the `appinstalled` event
